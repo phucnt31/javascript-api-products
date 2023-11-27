f2830ee34922096fbbc170f173aadb77a42b692e
phucnt31 <nguyenthienphuc.02@gmail.com>
2023-11-27 19:46:13 +0700

    Add query param

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ const displayProducts = (list) => {
       const { name: title, price } = product.fields;
       const { url: img } = product.fields.image[0];
       return `<!-- single product -->
-        <a class="single-product" href="product.html">
+        <a class="single-product" href="product.html?id=${id}">
             <img src="${img}" class="single-product-img img" alt="${title}">
             <footer>
               <h5 class="name">${title}</h5>
