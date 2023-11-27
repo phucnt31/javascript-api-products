3904519ec6b769a9e0dd74ff0f8b7c913f33ef6b
phucnt31 <nguyenthienphuc.02@gmail.com>
2023-11-27 19:17:18 +0700

    Add loading and error

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ const url = "https://course-api.com/javascript-store-products";
 const productsDOM = document.querySelector(".products-center");
 
 const fetchProducts = async () => {
+  productsDOM.innerHTML = '<div class="loading"></div>';
   try {
     const resp = await fetch(url);
     const data = await resp.json();
     console.log(data);
   } catch (error) {
     console.log(error);
+    productsDOM.innerHTML = '<p class="error">there was an error</p>';
   }
 };
 fetchProducts();
