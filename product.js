c3a2e4038a4c1a3cadaca5c60a843b61a4b78b7c
phucnt31 <nguyenthienphuc.02@gmail.com>
2023-11-27 19:58:22 +0700

    Fetch single product

--- product.js
+++ product.js
@@ -2,7 +2,19 @@ const url = "https://course-api.com/javascript-store-single-product";
 const productDOM = document.querySelector(".product");
 
 const fetchProduct = async () => {
-  return "product";
+  productDOM.innerHTML = '<h4 class="product-loading">Loading...</h4>';
+  try {
+    const params = new URLSearchParams(window.location.search);
+    const id = params.get("id");
+    // console.log(window.location.search);
+    const resp = await fetch(`${url}?id=${id}`);
+    const data = await resp.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+    productDOM.innerHTML =
+      '<p class="error">There was a problem loading the product. Please try again later </p>';
+  }
 };
 
 const displayProduct = (product) => {
